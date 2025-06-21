ec167585f571cf118d7c41e80a52231f88dfe06e
Seoyun <manaallaire@outlook.com>
2025-06-21 14:11:51 +0800

    feat: add missing coffee information
    Adds missing data (coffee size, extra espresso shot amount, and add-on syrup) required by the specifications (1.3 Dashboard Feature) and helpers for accessing the data.

--- src/main/java/cielsachen/ccprog3/model/Ingredient.java
+++ src/main/java/cielsachen/ccprog3/model/Ingredient.java
@@ -8,19 +8,25 @@ public enum Ingredient {
     MILK("Milk", 640, "fl oz"),
     WATER("Water", 640, "fl oz"),
 
-    HAZELNUT_SYRUP("Hazelnut Syrup", 640, "fl oz"),
-    CHOCOLATE_SYRUP("Chocolate Syrup", 640, "fl oz"),
-    ALMOND_SYRUP("Almond Syrup", 640, "fl oz"),
-    SWEETENER("Sweetener (Sucrose)", 640, "fl oz");
+    HAZELNUT_SYRUP("Hazelnut Syrup", true, 640, "fl oz"),
+    CHOCOLATE_SYRUP("Chocolate Syrup", true, 640, "fl oz"),
+    ALMOND_SYRUP("Almond Syrup", true, 640, "fl oz"),
+    SWEETENER("Sweetener (Sucrose)", true, 640, "fl oz");
 
+    public final boolean isSpecial;
     public final double maximumCapacity;
     public final String name;
     public final String unitMeasure;
 
-    private Ingredient(String name, double maximumCapacity, String unitMeasure) {
+    private Ingredient(String name, boolean isSpecial, double maximumCapacity, String unitMeasure) {
+        this.isSpecial = isSpecial;
         this.maximumCapacity = maximumCapacity;
         this.name = name;
         this.unitMeasure = unitMeasure;
+    }
+
+    private Ingredient(String name, double maximumCapacity, String unitMeasure) {
+        this(name, false, maximumCapacity, unitMeasure);
     }
 
     public String toMaximumCapacityString() {
--- src/main/java/cielsachen/ccprog3/model/transaction/Transaction.java
+++ src/main/java/cielsachen/ccprog3/model/transaction/Transaction.java
@@ -3,25 +3,44 @@ package cielsachen.ccprog3.model.transaction;
 import java.util.ArrayList;
 import java.util.List;
 
+import cielsachen.ccprog3.model.Ingredient;
+import cielsachen.ccprog3.model.coffee.CoffeeSize;
+
 public class Transaction {
     public final String coffeeName;
+    public final CoffeeSize coffeeSize;
     public final float cost;
+    public final int extraEspressoShotAmount;
     public final int truckId;
 
     private final List<TransactionIngredient> ingredients = new ArrayList<TransactionIngredient>();
 
-    public Transaction(String coffeeName, float cost, int truckId, TransactionIngredient... ingredients) {
+    public Transaction(String coffeeName, CoffeeSize coffeeSize, int extraEspressoShotAmount, float cost, int truckId,
+            TransactionIngredient... transactionIngredients) {
         this.coffeeName = coffeeName;
+        this.coffeeSize = coffeeSize;
         this.cost = cost;
+        this.extraEspressoShotAmount = extraEspressoShotAmount;
         this.truckId = truckId;
 
-        for (TransactionIngredient ingredient : ingredients) {
-            this.ingredients.add(ingredient);
+        for (TransactionIngredient transactionIngredient : transactionIngredients) {
+            this.ingredients.add(transactionIngredient);
         }
+    }
+
+    public Transaction(String coffeeName, CoffeeSize coffeeSize, float cost, int truckId,
+            TransactionIngredient... transactionIngredients) {
+        this(coffeeName, coffeeSize, 0, cost, truckId, transactionIngredients);
     }
 
     public List<TransactionIngredient> getIngredients() {
         return this.ingredients;
+    }
+
+    public List<Ingredient> getSyrups() {
+        return this.ingredients.stream()
+                .filter((transactionIngredient) -> transactionIngredient.ingredient.isSpecial)
+                .map((transactionIngredient) -> transactionIngredient.ingredient).toList();
     }
 
     public String toCostString() {
